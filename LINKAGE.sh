0cf2c378cb331569060c8ecee065a056751a6643
dex-ultima <58945119+dex-ultima@users.noreply.github.com>
2022-01-08 12:14:53 +0100

    try to fix bashrc

--- LINKAGE.sh
+++ LINKAGE.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "ZDOTDIR=$HOME/.config/zsh" > $HOME/.zshenv
-ln -s $HOME/.config/.bashrc $HOME/.bashrc
-ln -s $HOME/.config/.bash_profile $HOME/.bash_profile
+ln -sf $HOME/.config/.bashrc $HOME/.bashrc
+ln -sf $HOME/.config/.bash_profile $HOME/.bash_profile
